8e28ec1fa899aed67b2c86d447f92b358a58d523
Roy Wu <wuqiangroy@live.com>
2024-08-23 14:56:40 +0800

    opti: using DRF ORM as default query method

--- rides/serializers.py
+++ rides/serializers.py
@@ -1,6 +1,39 @@
 from rest_framework import serializers
 from django.core.validators import EmailValidator
 
+from rides.models import RideEvent, User, Ride
+
+
+class RideEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RideEvent
+        fields = ['id_ride_event', 'description', 'created_at']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id_user', 'role', 'first_name', 'last_name', 'email', 'phone_number']
+
+class RideSerializer(serializers.ModelSerializer):
+    rider = UserSerializer(source='id_rider')
+    driver = UserSerializer(source='id_driver')
+    today_ride_events = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Ride
+        fields = [
+            'id_ride', 'status', 'pickup_latitude', 'pickup_longitude',
+            'dropoff_latitude', 'dropoff_longitude', 'pickup_time',
+            'rider', 'driver', 'today_ride_events'
+        ]
+
+    def get_today_ride_events(self, obj):
+        ride_event_map = self.context.get('ride_event_map', {})
+        events = ride_event_map.get(obj.id_ride, [])
+        return RideEventSerializer(events, many=True).data
+
+
+
 
 class RideListSerializer(serializers.Serializer):
     status = serializers.ChoiceField(required=False, choices=["en-route", "pickup", "dropoff"])
--- rides/views.py
+++ rides/views.py
@@ -1,108 +1,96 @@
+from datetime import timedelta
+
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework.response import Response
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import OrderingFilter, SearchFilter
 
-from django.db import connection
+from django.utils import timezone
+from django.db.models import F, ExpressionWrapper, FloatField
 
-from rides.models import User, Ride
-from rides.serializers import RideListSerializer, UserLoginSerializer
+from rides.models import User, Ride, RideEvent
+from rides.serializers import RideListSerializer, UserLoginSerializer, RideSerializer
 from rides.permissions import IsAdmin
+from rides.exceptions import CustomValidationError
+
+
+class RideFilter(django_filters.FilterSet):
+    status = django_filters.CharFilter(lookup_expr='icontains')
+    rider_email = django_filters.CharFilter(field_name='id_rider__email', lookup_expr='icontains')
+
+    class Meta:
+        model = Ride
+        fields = ['status', 'rider_email']
+
+
+class RidePagination(PageNumberPagination):
+    page_size = 10  
 
 
 class RideViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
-    queryset = Ride.objects.all()
+    queryset = Ride.objects.select_related().all()
     permission_classes = [IsAuthenticated, IsAdmin]
+    serializer_class = RideSerializer
+    pagination_class = RidePagination
+    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
+    filterset_class = RideFilter
+    ordering_fields = ['pickup_time']
+
+    def get_queryset(self):
+        serializer = RideListSerializer(data=self.request.query_params)
+        if not serializer.is_valid():
+            raise CustomValidationError(400, "invalid input parameters.")
+
+        validated_data = serializer.validated_data
+        queryset = super().get_queryset()
+
+        lat = validated_data.get('latitude')
+        lon = validated_data.get('longitude')
+
+        # first query
+        if lat and lon:
+            queryset = queryset.annotate(
+                distance=ExpressionWrapper(
+                    (F('pickup_latitude') - lat) ** 2 + 
+                    (F('pickup_longitude') - lon) ** 2,
+                    output_field=FloatField()
+                )
+            ).order_by('pickup_time', 'distance')
+
+        return queryset
 
     def list(self, request, *args, **kwargs):
-        serializer = RideListSerializer(data=request.query_params)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=400)
-        
-        lat, lon = serializer.validated_data.get('latitude'), serializer.validated_data.get('longitude')
-        status, rider_email = serializer.validated_data.get('status'), serializer.validated_data.get('rider_email')
-        page = serializer.validated_data.get('page')
-        
-        query = """
-            SELECT r.id_ride, r.status, r.pickup_latitude, r.pickup_longitude, r.dropoff_latitude, r.dropoff_longitude, r.pickup_time, 
-                u1.id_user, u1.role, u1.first_name, u1.last_name, u1.email, u1.phone_number,
-                u2.id_user, u2.role, u2.first_name, u2.last_name, u2.email, u2.phone_number
-            """
-        order = " ORDER BY r.pickup_time ASC"
-        
-        if lat and lon:
-            query += f" ,((r.pickup_latitude - {lat}) * (r.pickup_latitude - {lat}) + (r.pickup_longitude - {lon}) * (r.pickup_longitude - {lon})) AS distance"
-            order += ", distance ASC"
-        
-        query += " FROM rides_ride r LEFT JOIN rides_user u1 ON r.id_rider_id = u1.id_user LEFT JOIN rides_user u2 ON r.id_driver_id = u2.id_user"
-        
-        conditions = []
-        if status:
-            conditions.append(f"r.status = '{status}'")
-        if rider_email:
-            conditions.append(f"u1.email = '{rider_email}'")
-        if conditions:
-            query += " WHERE " + " AND ".join(conditions)
-        
-        count_query = f"SELECT COUNT(1) as count FROM ({query}) AS subquery"
-        with connection.cursor() as cursor:
-            cursor.execute(count_query)
-            total_count = cursor.fetchone()[0]
-        
-        query += order
-        limit = 10
-        offset = limit * (page - 1)
-        query += f" LIMIT {limit} OFFSET {offset}"
+        queryset = self.filter_queryset(self.get_queryset())
 
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            results = cursor.fetchall()
-        in_condition = ",".join([str(i[0]) for i in results])
-        event_query = f"SELECT * FROM rides_rideevent re Where re.id_ride_id IN ({in_condition}) AND re.created_at > datetime('now', '-1 day');"
-        with connection.cursor() as cursor:
-            cursor.execute(event_query)
-            events = cursor.fetchall()
+        # second query
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            ride_ids = [ride.id_ride for ride in page]
+            # third query
+            ride_events = RideEvent.objects.select_related().filter(
+                id_ride__in=ride_ids,
+                created_at__gt=timezone.now() - timedelta(days=1)
+            )
 
-        events_mapping = {}
-        for e in events:
-            if e[0] in events_mapping:
-                events_mapping[e[0]].append({'id_ride_event':e[1], 'description':e[2], 'created_at':e[3]})
-            else:
-                events_mapping[e[0]] = [{'id_ride_event':e[1], 'description':e[2], 'created_at':e[3]}]
-        res = [{
-            'id_ride': i[0],
-            'status': i[1],
-            'pickup_latitude': i[2],
-            'pickup_longitude': i[3],
-            'dropoff_latitude': i[4],
-            'dropoff_longitude': i[5],
-            'pickup_time': i[6],
-            'rider': {
-                'id_user': i[7],
-                'role': i[8], 
-                'first_name': i[9], 
-                'last_name': i[10], 
-                'email': i[11], 
-                'phone_number': i[12]
-            },
-            "driver": {
-                'id_user': i[13],
-                'role': i[14], 
-                'first_name': i[15], 
-                'last_name': i[16], 
-                'email': i[17], 
-                'phone_number': i[18]
-            },
-            'today_ride_events': events_mapping[i[0]] if i[0] in events_mapping else []
-        } for i in results]
+            ride_event_map = {}
+            for event in ride_events:
+                if event.id_ride in ride_event_map:
+                    ride_event_map[event.id_ride].append(event)
+                else:
+                    ride_event_map[event.id_ride] = [event]
 
-        return Response({
-            'total': total_count,
-            'next': None if 10 * page > total_count else page+1,
-            'prev': None if page == 1 else page-1,
-            'results': res,
-            })
+            serializer = self.get_serializer(page, many=True, context={'ride_event_map': ride_event_map})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class LoginView(generics.GenericAPIView):
 
